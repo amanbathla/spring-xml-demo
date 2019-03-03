241b201f66a90a2bb717732f982bb49f83fa8ba0
amabath1 <amabath1@in.ibm.com>
2019-03-03 22:32:35 +0530

    bean Post Processor

--- src/main/java/com/stackRoute/BeanPostProcessorDemoBean.java
+++ src/main/java/com/stackRoute/BeanPostProcessorDemoBean.java
@@ -1,4 +1,17 @@
 package com.stackRoute;
 
-public class BeanPostProcessorDemoBean {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("In Before Init Bean with BeanName :" + beanName);
+        return  bean;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("In After Init Bean with BeanName :" + beanName);
+        return  bean;
+    }
 }
